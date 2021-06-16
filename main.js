3357190490898d0fb163a31ebcbbcebd0711ffa2
Catlerc <mcr1123@yandex.ru>
2021-06-17 00:54:37 +0500

    local image loading test2

--- main.js
+++ main.js
@@ -1,30 +1,48 @@
 var canvas = new fabric.Canvas('c')
 
 fabric.Object.prototype.transparentCorners = false;
-canvas.setBackgroundColor('rgb(254,254,254)', null);
 
+var can =document.getElementById('c')
 var input = document.getElementById('file-input')
 input.addEventListener('change', function (event) {
     const fileList = event.target.files;
     var file = fileList.item(0);
 
-    canvas.clear();
+
 
     var reader = new FileReader();
     reader.onloadend = function() {
         fabric.Image.fromURL(reader.result, function (img) {
-            var img2 = img.set({left: 175, top: 175});
-            canvas.add(img2);
-
+            canvas.clear();
+            canvas.setBackgroundColor('rgb(254,254,254)', null);
+            canvas.add(img.set({left: 175, top: 175}));
             canvas.renderAll();
 
             var gif = new GIF({
                 workers: 2,
                 quality: 10,
-                //transparent: 0xFEFEFE
+                transparent: 0xFEFEFE
             });
+            var frame1 = document.createElement('img');
+            frame1.width=300;
+            frame1.height=300;
+            frame1.src = can.toDataURL();
+            gif.addFrame(frame1, {delay: 300});
 
-            gif.addFrame(document.getElementById('c'));
+
+
+            ////
+
+            canvas.clear();
+            canvas.setBackgroundColor('rgb(254,254,254)', null);
+            canvas.add(img.set({left: 75, top: 175}));
+            canvas.renderAll();
+            var frame2 = document.createElement('img');
+            frame2.src = can.toDataURL();
+            frame2.width=300;
+            frame2.height=300;
+            gif.addFrame(frame2, {delay: 300});
+            //
 
             gif.on('finished', function (blob) {
                 document.getElementById('mg').src = URL.createObjectURL(blob);
